54c7feea7312bc926c3af88f8fb6f415fde616fc
sy <shiyung@gmail.com>
2018-05-04 12:19:52 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pg-slack-notifier
-Positive Grid slack channel notifier by incoming webhook.
+# slack-easy-webhook
+Just a simple wrapper which make slack incoming webhook more easy to use (and don't need to install any dependency) with Promise.
+
+## Install
+
+```
+npm install slack-easy-webhook
+```
+
+## Usage
+
+```js
+const slack = require('slack-easy-webhook')
+
+slack(WEBHOOK_URL, OPTS)
+```
+
+### Options
+All Slack accepts options ([slack incoming webhook][incoming-webhook-doc], like `username`, `channel`, `icon_url`, `icon_emoji`, `attachments`) and its [message formatting][slack-message-formatting] or [message attachments][slack-message-attachments].
+
+[incoming-webhook-doc]: https://api.slack.com/incoming-webhooks
+[slack-message-formatting]: https://api.slack.com/docs/formatting
+[slack-message-attachments]: https://api.slack.com/docs/attachments
--- index.js
+++ index.js
@@ -1,14 +1,45 @@
 'use strict'
 
-const http = require('http')
+const url = require('url')
 const https = require('https')
 
-function pgSlackNotifier (opts) {
+function slackEasyWebhook (link, opts) {
   return new Promise((resolve, reject) => {
-    if (typeof opts !== 'object') {
-      return reject(new Error('Missing necessary slack webhook parameters'))
+    if (typeof opts !== 'object' || typeof link !== 'string') {
+      return reject(new Error('Missing necessary input parameters'))
     }
+
+    const notPass = ['channel', 'text'].some(i => {
+      return opts.hasOwnProperty(i) === false
+    })
+    if (notPass) {
+      return reject(Error('channel & text are necessary parameters'))
+    }
+
+    const postBody = JSON.stringify(opts)
+
+    const webhookURL = url.parse(link)
+    const req = new https.request({
+      protocol: webhookURL.protocol,
+      hostname: webhookURL.hostname,
+      path: webhookURL.pathname,
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(postBody)
+      }
+    }, (res) => {
+      res.on('data', (d) => {
+        return resolve(Buffer.from(d).toString('utf-8'))
+      })
+    })
+    req.write(postBody)
+    req.end()
+
+    req.on('error', (err) => {
+      return reject(err) 
+    })
   })
 }
 
-module.exports = pgSlackNotifier
+module.exports = slackEasyWebhook
